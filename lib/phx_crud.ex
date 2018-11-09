42001fc4d1a51d7d1aea62243a06bc05990a9491
Tanweer Shahzaad <tanweerdev@gmail.com>
2018-11-09 17:41:15 +0500

    Initial commit

---
+++ lib/phx_crud.ex
@@ -0,0 +1,18 @@
+defmodule PhxCrud do
+  @moduledoc """
+  Documentation for PhxCrud.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhxCrud.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phx_crud_test.exs
@@ -0,0 +1,8 @@
+defmodule PhxCrudTest do
+  use ExUnit.Case
+  doctest PhxCrud
+
+  test "greets the world" do
+    assert PhxCrud.hello() == :world
+  end
+end
