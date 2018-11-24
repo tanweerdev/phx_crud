9efde726b8a9862509e729e6770e9c4e08ed366f
Tanweer Shahzaad <tanweerdev@gmail.com>
2018-11-24 18:35:55 +0500

    Update mix file for publish

--- mix.exs
+++ mix.exs
@@ -7,7 +7,18 @@ defmodule PhxCrud.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      build_embedded: Mix.env() == :prod,
+      deps: deps(),
+      name: "PhxCrud",
+      description: description(),
+      package: package(),
+      docs: [
+        # The main page in the docs
+        main: "readme",
+        # logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ],
+      source_url: "https://github.com/tanweerdev/phx_crud"
     ]
   end
 
@@ -21,9 +32,25 @@ defmodule PhxCrud.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 2.2"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:earmark, "~> 1.2", only: :dev}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description() do
+    "phx_crud provides standard macros for common phoenix crud operations"
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/tanweerdev/phx_crud",
+        "Docs" => "https://hexdocs.pm/phx_crud/"
+      }
     ]
   end
 end
